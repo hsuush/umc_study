1a8b8a755d2404b99e5db1351b4a326e3dbf1a05
hsuush <dlhyes703@naver.com>
2023-11-18 22:34:33 +0900

    #1 [FEAT] 지역에 가게추가 API 구현

--- src/main/java/umc5th/spring/web/controller/StoreRestController.java
+++ src/main/java/umc5th/spring/web/controller/StoreRestController.java
@@ -3,22 +3,16 @@ package umc5th.spring.web.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import umc5th.spring.apiPayload.ApiResponse;
-import umc5th.spring.converter.MemberConverter;
 import umc5th.spring.converter.StoreConverter;
-import umc5th.spring.domain.Member;
 import umc5th.spring.domain.Store;
 import umc5th.spring.service.StoreService.StoreCommandService;
-import umc5th.spring.web.dto.MemberRequestDTO;
-import umc5th.spring.web.dto.MemberResponseDTO;
 import umc5th.spring.web.dto.StoreRequestDTO;
 import umc5th.spring.web.dto.StoreResponseDTO;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/stores")
 @RequiredArgsConstructor
-public class StoreController {
+public class StoreRestController {
 
     private final StoreCommandService storeCommandService;
 
